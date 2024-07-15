35dd5b0137dd897885471db6908f3af1adaa22fe
praneethhh18 <140519419+praneethhh18@users.noreply.github.com>
2024-07-15 23:25:43 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# my-repo
+# Starter Repo
+This repo has everything you need to get started on the program, good luck!
--- car.py
+++ car.py
@@ -1,11 +1,10 @@
-from serviceable import Serviceable
+from abc import ABC, abstractmethod
 
 
-class Car(Serviceable):
-    def __init__(self, engine, battery, tires):
-        self.engine = engine
-        self.battery = battery
-        self.tires = tires
+class Car(ABC):
+    def __init__(self, last_service_date):
+        self.last_service_date = last_service_date
 
+    @abstractmethod
     def needs_service(self):
-        return self.engine.needs_service() or self.battery.needs_service() or self.tires.needs_service()
+        pass
